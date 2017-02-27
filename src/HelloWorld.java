238cbf032d17c0da37dd0a47e603b138c366e119
Amin Javed <ajaved@student.tgm.ac.at>
2017-02-27 11:12:10 +0100

    8xHelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,10 @@ public class HelloWorld {
 	
    public static void main(String[] args) {
 	   
-	  for ( int i = 1 ; i <= 5 ; i++ ) {
+	  for ( int i = 1 ; i <= 8 ; i++ ) {
+		  System.out.println("Hello, World + i");
+		  System.out.println("Hello, World + i");
+		  System.out.println("Hello, World + i");
 		  System.out.println("Hello, World + i");
 		  System.out.println("Hello, World + i");
 		  System.out.println("Hello, World + i");
